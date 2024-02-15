b7b52493532a810972b124363eeee3182d210752
Angmar3019 <info@vockensohn.de>
2024-02-15 23:48:32 +0100

    Fix publisher

--- Uebung 01/publisher.py
+++ Uebung 01/publisher.py
@@ -1,22 +1,22 @@
 import paho.mqtt.client as mqtt
 import time
 import random
+import uuid
+import json
 
-mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, "23875438", clean_session=False)
+mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, uuid.uuid4().hex, clean_session=False)
 mqttc.connect("broker.hivemq.com", 1883, 60)
 
 while True:
     data = {
-    
-    "fin" : "SNTU411STM5032980",
-    "zeit" : int (time.time()),
-    "geschwindigkeit" : int (random.randrange(0, 50, 1))
-    
+        "fin" : "SNTU411STM5032980",
+        "zeit" : int (time.time()),
+        "geschwindigkeit" : int (random.randint(0, 50))
     }
+
+    data = json.dumps(data)
     print(data)
-    print(str (data))
-    
-    mqttc.publish("DataMgmt", str (data), qos=1)
+    mqttc.publish("DataMgmt", str(data), qos=1)
     
     time.sleep(5)
     
