188cb78c6114226fc0bd8f8803fe047cddeef278
Angmar3019 <info@vockensohn.de>
2024-02-15 23:48:40 +0100

    Fix subscriber

--- Uebung 01/subscriber.py
+++ Uebung 01/subscriber.py
@@ -1,8 +1,10 @@
 import paho.mqtt.client as mqtt
 import psycopg2
-from dotenv import load_dotenv
 import os
+import uuid
 import json
+
+from dotenv import load_dotenv
 
 # Lädt die Umgebungsvariablen aus der .env-Datei
 load_dotenv()
@@ -12,7 +14,6 @@ db_host = os.getenv("DB_HOST")
 db_name = os.getenv("DB_NAME")
 db_user = os.getenv("DB_USER")
 db_password = os.getenv("DB_PASSWORD")
-
 
 # Verbindung zur Datenbank herstellen
 def connect_db():
@@ -28,21 +29,20 @@ def connect_db():
 def insert_into_database(message):
     conn = connect_db()
     cur = conn.cursor()
-    try:
-        cur.execute("INSERT INTO staging.messung (payload) VALUES (%s)", (message,))
-        conn.commit()
-    except Exception as e:
-        print(f"Fehler beim Einfügen in die Datenbank: {e}")
-    finally:
-        cur.close()
-        conn.close()
 
+    cur.execute("INSERT INTO staging.messung (payload) VALUES (%s)", (message,))
+    conn.commit()
+
+    cur.close()
+    conn.close()
 
 def on_message(client, userdata, message):
     message = message.payload.decode()
+    #message = json.dumps(message)
+    print(message)
     insert_into_database(message)
 
-mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, "23875439", clean_session=False)
+mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, uuid.uuid4().hex, clean_session=False)
 mqttc.on_message = on_message
 mqttc.connect("broker.hivemq.com", 1883, 60)
 mqttc.subscribe("DataMgmt", qos=1)
